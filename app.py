4566ce59948f1d32b6bbad0d2a55a7aa3c176e41
Chris Whittle <cwhittl@gmail.com>
2018-12-29 15:59:38 -0600

    wip

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # ./app.py
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +10,25 @@ def index():
 @app.route('/info')
 def info():
 	return "Info!"
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == "__main__":
 	app.run()
